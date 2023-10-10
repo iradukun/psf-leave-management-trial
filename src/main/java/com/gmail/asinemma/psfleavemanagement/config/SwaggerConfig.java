26f8a6ed02220aa57d06b13510a4a7fc41dde566
iradukun <mustaphairadukunda@gmail.com>
2023-10-10 14:49:31 +0200

    commenting swagger to solve

--- src/main/java/com/gmail/asinemma/psfleavemanagement/config/SwaggerConfig.java
+++ src/main/java/com/gmail/asinemma/psfleavemanagement/config/SwaggerConfig.java
@@ -1,25 +1,25 @@
-package com.gmail.asinemma.psfleavemanagement.config;
+// package com.gmail.asinemma.psfleavemanagement.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+// import springfox.documentation.builders.PathSelectors;
+// import springfox.documentation.builders.RequestHandlerSelectors;
+// import springfox.documentation.spi.DocumentationType;
+// import springfox.documentation.spring.web.plugins.Docket;
+// import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
+// @Configuration
+// @EnableSwagger2
+// public class SwaggerConfig {
 
-@Bean
-public Docket organizationManagementApi() {
-return new Docket(DocumentationType.SWAGGER_2)
-.select()
-.apis(RequestHandlerSelectors.basePackage("com.gmail.asinemma.psfleavemanagement.controllers"))
-.paths(PathSelectors.ant("/psf/leaves.*"))
-.build();
-}
+// @Bean
+// public Docket organizationManagementApi() {
+// return new Docket(DocumentationType.SWAGGER_2)
+// .select()
+// .apis(RequestHandlerSelectors.basePackage("com.gmail.asinemma.psfleavemanagement.controllers"))
+// .paths(PathSelectors.ant("/psf/leaves.*"))
+// .build();
+// }
 
-}
+// }
